73e2140cc99b72724d433d030c671b6a5dd93d71
liujiawen <liujiawen.personal@outlook.com>
2020-12-21 11:34:01 +0800

    redis修改

--- application/common/expand/RedisUtils.php
+++ application/common/expand/RedisUtils.php
@@ -4,76 +4,100 @@ namespace app\common\expand;
 
 final class RedisUtils
 {
-    private object $handler;
-    private static object $static_handler;
-    // 自身实例
-    private static $instance;
-    private array $config = [
-        'host'       => '127.0.0.1',
-        'port'       => 6379,
-        'password'   => '',
-        'timeout'    => 1.5,
-        'persistent' => true,
-        'select'     => 0,
-        'prefix'     => ''
-    ];
+    private static $_instance;
+    private static $k;
+    private $redis;
+    // 重连间隔时间
+    private $expire_time = 60;
+    private $host        = 'localhost';
+    private $port        = '6379';
+    //当前数据库ID号
+    private static $db_id = 0;
+    //当前权限认证码
+    private $auth = '';
 
-    /**
-     * 单机连接
-     * @access private
-     * @param Array $config = []
-     */
-    private function __construct(array $config = [])
+    private function __clone()
     {
-        // 优先从配置文件读取配置
-        $config = !empty($config) ? $config : config('cache.redis', $this->config);
+    }
 
-        // 合并配置
-        if (!empty($config)) {
-            $this->config = array_merge($this->config, $config);
-        }
+    public function __destruct()
+    {
+        return static::$_instance[self::$k]->redis->close();
+    }
 
-        // [PhpRedis](https://github.com/phpredis/phpredis)
-        if (extension_loaded('redis') && !isset($this->handler)) {
-            $this->handler = new \Redis();
+    private function __construct(array $config)
+    {
+        (!isset($config['host']) || !$config['host'] || $config['host'] == $this->host) ?: $this->host = $config['host'];
+        (!isset($config['port']) || !$config['port'] || $config['port'] == $this->port) ?: $this->port = $config['port'];
+        (!isset($config['auth']) || !$config['auth']) ?: $this->auth  = $config['auth'];
+        (!isset($config['timeout']) || !$config['timeout']) ?: $this->expire_time  = time() + $config['timeout'];
+
+        if (extension_loaded('redis') && !isset($this->redis)) {
+            // [PhpRedis](https://github.com/phpredis/phpredis)
+            $this->redis = new \Redis();
             // 长连接；依赖于 php-fpm 进程，php-fpm进程不死，redis connect 就一直存在，直到空闲超时自动断开
-            if ($this->config['persistent']) {
-                $this->handler->pconnect($this->config['host'], $this->config['port'], $this->config['timeout'], 'persistent_id_' . $this->config['select']);
+            if (isset($config['persistent']) && $config['persistent']) {
+                $this->redis->pconnect($this->host, $this->port, $config['timeout'], 'persistent_id_' . self::$db_id);
             } else {
-                $this->handler->connect($this->config['host'], $this->config['port'], $this->config['timeout']);
+                $this->redis->connect($this->host, $this->port, $config['timeout']);
             }
-
-            !$this->config['password'] ?: $this->handler->auth($this->config['password']);
-            // 选择数据表
-            (0 == (int)$this->config['select']) ?: $this->handler->select($this->config['select']);
-
-            self::$static_handler = &$this->handler;
-            return $this;
+        } elseif (class_exists('\\Predis\\Client') && !isset($this->redis)) {
+            // [predis](https://github.com/predis/predis)
+            $this->redis = new \Predis\Client($this->config);
         } else {
             throw new \Exception('not support: redis');
         }
-
-        // [predis](https://github.com/predis/predis)
-        if (class_exists('\\Predis\\Client') && !isset($this->handler)) {
-            self::$static_handler = $this->handler = new \Predis\Client($this->config);
-            return $this;
-        } else {
-            throw new \Exception('not support: redis');
-        }
+        !$this->auth ?: $this->redis->auth($this->auth);
+        !self::$db_id ?: $this->select(self::$db_id);
     }
 
     /**
-     * 获取 Redis 实例
-     * @param $config = []
-     * @return Self
+     * 得到实例化的对象.
+     * 为每个数据库建立一个连接
+     * 如果连接超时，将会重新建立一个连接
+     * @param array $config
+     * @param int $db_id
+     * @return \iphp\db\Redis
      */
-    public static function getInstance($config = []): self
+    public static function getInstance(array $config = [], int $db_id = 0): self
     {
-        //判断实例有无创建，没有的话创建实例并返回，有的话直接返回
-        if (!(self::$instance instanceof self)) {
-            self::$instance = new self($config);
+        !$db_id ?: self::$db_id = $config['select'] = $db_id;
+
+        self::$k = $k = md5(implode('', $config));
+
+        $instance = &static::$_instance[$k];
+
+        if (!($instance instanceof self)) {
+            $instance         = new self($config);
+            $instance::$db_id = $db_id;
+        } elseif (time() > $instance->expire_time) {
+            $instance->close();
+            $instance         = new self($config);
+            $instance::$db_id = $db_id;
         }
-        return self::$instance;
+        return static::$_instance[$k];
+    }
+
+    /**
+     * @access public
+     * @param String $method_name
+     * @param Mixed $arguments
+     * @return Mixed
+     */
+    public static function __callStatic($method_name, $arguments)
+    {
+        return call_user_func([static::$_instance[self::$k]->redis, $method_name], ...$arguments);
+    }
+
+    /**
+     * @access public
+     * @param String $method_name
+     * @param Mixed $arguments
+     * @return Mixed
+     */
+    public function __call($method_name, $arguments)
+    {
+        return call_user_func([static::$_instance[self::$k]->redis, $method_name], ...$arguments);
     }
 
     /**
@@ -86,20 +110,23 @@ final class RedisUtils
     public function RefreshExpireTime(String $key, int $seconds): int
     {
         // 剩余时间
-        $remain_time = $this->handler->ttl($key);
-        $code = 1;
+        $remain_time = $this->ttl($key);
+        $code        = 1;
         switch ($remain_time) {
             case -1:
                 // 存在 key 但没有设置剩余过期时间
-                $this->handler->expire($key, $seconds);
+                $this->expire($key, $seconds);
                 $code = -1;
+                // no break
             case -2:
                 // 不存在 key
                 $code = -2;
+                // no break
             case $remain_time < ($seconds * (2 / 3)):
                 // 剩余时间小于给定时间的 2/3
-                $this->handler->expire($key, $seconds);
+                $this->expire($key, $seconds);
                 $code = 1;
+                // no break
             default:
                 $code = 1;
         }
@@ -107,30 +134,120 @@ final class RedisUtils
     }
 
     /**
-     * @access public
-     * @param String $method_name
-     * @param Mixed $arguments
-     * @return Mixed
+     * 删除hash表中指定字段 ,支持批量删除
+     * @param string $key 缓存key
+     * @param string  $field 字段
+     * @return int
      */
-    public static function __callStatic($method_name, $arguments)
+    public function hdel(string $key, string $field)
     {
-        // return self::$static_handler->$method_name($arguments);
-        return call_user_func([self::$static_handler, $method_name], $arguments);
+        $field_arr = explode(',', $field);
+        $del_num   = 0;
+
+        foreach ($field_arr as $row) {
+            $row      = trim($row);
+            $del_num += $this->redis->hdel($key, $row);
+        }
+        return $del_num;
     }
 
     /**
-     * @access public
-     * @param String $method_name
-     * @param Mixed $arguments
-     * @return Mixed
+     * 选择数据库
+     * @param int $db_id 数据库ID号
      */
-    public function __call($method_name, $arguments)
+    public function select($db_id): bool
     {
-        // return $this->handler->$method_name($arguments);
-        return call_user_func([$this->handler, $method_name], $arguments);
+        self::$db_id = $db_id;
+        return $this->redis->select($db_id);
     }
 
-    private function __clone()
+    /**
+     * 关闭所有连接
+     */
+    public static function closeAll(): void
     {
+        foreach (static::$_instance as $o) {
+            if ($o instanceof self) {
+                $o->close();
+            }
+        }
+    }
+
+    /**
+     * 得到当前数据库ID
+     * @return int
+     */
+    public function getDbId()
+    {
+        return self::$db_id;
+    }
+
+    public function getAuth()
+    {
+        return $this->auth;
+    }
+
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    public function getConfig()
+    {
+        return [
+            'host' => $this->host,
+            'port' => $this->port,
+            'auth' => $this->auth
+        ];
+    }
+
+    /**
+     * 得到一组的 hash 数据的值
+     * @param String $prefix
+     * @param String|Array $ids
+     */
+    public function hashAll(string $prefix, $ids)
+    {
+        if (!$ids) {
+            return [];
+        }
+        if (is_string($ids)) {
+            $ids = explode(',', $ids);
+        }
+        $arr = [];
+        foreach ($ids as $id) {
+            $key = $prefix . '.' . $id;
+            $res = $this->redis->hgetall($key);
+            if ($res != false) {
+                $arr[$key] = $res;
+            }
+        }
+        return $arr;
+    }
+
+    /**
+     * 得到条批量删除key的命令
+     * @param String $keys
+     * @param Integer|String $db_id
+     */
+    public function delKeys(string $keys, $db_id = 0): string
+    {
+        $redis_info = $this->getConfig();
+
+        $cmd_arr = [
+            'redis-cli', '-a',
+            $redis_info['auth'], '-h',
+            $redis_info['host'], '-p',
+            $redis_info['port'], '-n',
+            $db_id,
+        ];
+
+        $redis_str = implode(' ', $cmd_arr);
+        return "{$redis_str} KEYS \"{$keys}\" | xargs {$redis_str} del";
     }
 }
